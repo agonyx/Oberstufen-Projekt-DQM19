10a18d31ad51c3253afea87d7283909e8b3d486a
agonyx <raven28@gmx.de>
2022-01-27 10:55:30 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 }
 // hithrtz
+//Leon ist cool
