5679c87ef177fc2acad3da233b727ade55392287
BlackStorry <franco@groegl.de>
2022-02-10 10:25:52 +0100

    1 file modified

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { MainMenuComponent } from '../components/main-menu/main-menu.component';
+import { PlayerComponent } from '../components/player/player.component';
 
-const routes: Routes = []; // sets up routes constant where you define your routes
+const routes: Routes = [
+  { path: '', redirectTo: '/mainmenu', pathMatch: 'full' },
+  { path: 'mainmenu', component: MainMenuComponent },
+  { path: 'player/:id', component:  PlayerComponent}
+]; // sets up routes constant where you define your routes
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
