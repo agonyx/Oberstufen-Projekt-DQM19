31582ce282f51b271ea5fae3240e07e16381cf0f
agonyx <60823489+agonyx@users.noreply.github.com>
2022-02-10 10:15:50 +0100

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
+import { MessageComponent } from './message/message.component';
+import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ChatroomComponent, MessageComponent, UserItemComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
