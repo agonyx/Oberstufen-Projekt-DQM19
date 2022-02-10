99c72a19eb7c015a0dbace6b76c9855600d3f648
BlackStorry <franco@groegl.de>
2022-02-10 10:46:08 +0100

    1 file modified, 1 added

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -7,7 +7,7 @@ import { PlayerComponent } from '../components/player/player.component';
 const routes: Routes = [
   { path: '', redirectTo: '/mainmenu', pathMatch: 'full' },
   { path: 'mainmenu', component: MainMenuComponent },
-  { path: 'player/:id', component:  PlayerComponent}
+  { path: 'player', component:  PlayerComponent}
 ]; // sets up routes constant where you define your routes
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
