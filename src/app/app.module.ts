97ac673f1696486fb69a4de8d5b32df08edaf7c7
agonyx <60823489+agonyx@users.noreply.github.com>
2022-02-24 08:35:12 +0100

    swag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,17 @@ import { HelloComponent } from './hello.component';
 import { ChatroomComponent } from './chatroom/chatroom.component';
 import { MessageComponent } from './message/message.component';
 import { UserItemComponent } from './user-item/user-item.component';
+
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import {ChatService} from "./services/chat.service";
+import { UserListComponent } from './user-list/user-list.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AngularFireModule, AngularFireDatabaseModule ],
-  declarations: [ AppComponent, HelloComponent, ChatroomComponent, MessageComponent, UserItemComponent ],
+  declarations: [ AppComponent, HelloComponent, ChatroomComponent, MessageComponent, UserItemComponent, UserListComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ChatService]
 })
