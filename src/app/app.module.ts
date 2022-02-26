c8db3eeb99967399526c74f72cc60106a32a7852
agonyx <60823489+agonyx@users.noreply.github.com>
2022-02-26 09:10:31 +0100

    added popperjs, Bootstrap and Angular Bootstrap Widgets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,14 @@ import { ChatroomComponent } from './components/chatroom/chatroom.component';
 import { MessageComponent } from './components/message/message.component';
 import { UserItemComponent } from './components/user-item/user-item.component';
 
-import {ChatService} from "./services/chat.service";
+import { ChatService } from "./services/chat.service";
 import { UserListComponent } from './components/user-list/user-list.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { AppRoutingModule } from './components/app-routing/app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,  AppRoutingModule ],
+  imports:      [ BrowserModule, FormsModule,  AppRoutingModule, NgbModule ],
   declarations: [ AppComponent, MainMenuComponent, ChatroomComponent, MessageComponent, UserItemComponent, UserListComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ChatService]
