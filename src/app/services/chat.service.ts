48e1b7da16b8a011a38cc4dd0af2a9a00c0157d0
agonyx <60823489+agonyx@users.noreply.github.com>
2022-02-26 13:30:02 +0100

    fixed firebase and @angular/fire compatability

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
 import {ChatMessage} from "../models/chat-message.model";
+import {AngularFireDatabase, AngularFireList} from "@angular/fire/compat/database";
 @Injectable({
   providedIn: 'root'
 })
