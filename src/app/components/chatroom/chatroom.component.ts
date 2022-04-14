b401e970cfe999376d7a50985ec05961cecce17d
agonyx <60823489+agonyx@users.noreply.github.com>
2022-04-14 19:46:50 +0200

    Würfeln Part 1
    HTML Basis

--- src/app/components/chatroom/chatroom.component.ts
+++ src/app/components/chatroom/chatroom.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-chatroom',
   templateUrl: './chatroom.component.html',
   styleUrls: ['./chatroom.component.css']
 })
 export class ChatroomComponent implements OnInit {
-
+wuerfelwunsch = false;
   constructor() { }
 
   ngOnInit(): void {
   }
+switchWuerfelwunschState(){
+    if(this.wuerfelwunsch) {
+      this.wuerfelwunsch = false;
+    }else if (!this.wuerfelwunsch){
+      this.wuerfelwunsch = true;
+    }
+}
+
+  randomBetween(min: number, max: number):number {
+  return Math.round(Math.random() * (max - min)+ min);
+}
 
 }
